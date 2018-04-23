e80288368fcd3a571b1aed1ef9015bd75c22fcc2
makotokanazawa <37959854+makotokanazawa@users.noreply.github.com>
2018-04-23 15:04:12 +0900

    Update jsonesharp.js
    Undo changes

--- jsonesharp.js
+++ jsonesharp.js
@@ -25,18 +25,6 @@ var halting_message = function(n, p) {
 
 };
 
-var halting_message_steps = function(n, p, m) {
-
-    //TODO: Look into more informative halting messages
-    if (n == p.length) {
-	   status_text('halted properly after '.concat(String(m),' steps');
-    }
-    else {
-	   status_text('halted improperly after '.concat(String(m),' steps');
-    }
-
-};
-
 var eval_message = function() {
 
     status_text('evaluating program');
@@ -299,7 +287,6 @@ var eval_slow = function() {
 
 var pos = 0;
 var halted = 0;
-var n_steps = 0;
 
 var eval_step = function() {
 
@@ -314,12 +301,10 @@ var eval_step = function() {
 
     var new_pos = step(p, pos, regs);
     if (new_pos == pos) {
-	halting_message_steps(pos, p, n_steps);
+	halting_message(pos, p);
 	halted = 1;
     } else {
 	pos = new_pos;
-	n_steps++;
-	status_test('evaluating program -- executed '.concat(String(n_steps),' steps'));
     };
     array_to_dom_regs(regs);
     if (!halted) {
@@ -330,7 +315,6 @@ var eval_step = function() {
 var reset_program = function() {
     pos = 0;
     halted = 0;
-    n_steps = 0;
     eval_button_ready();
     status_text('a 1# interpreter for web browsers');
 }
