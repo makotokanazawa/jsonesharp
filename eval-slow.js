7d190787a542cc4fd6a72c4bdf780de8b1dd00cf
makotokanazawa <37959854+makotokanazawa@users.noreply.github.com>
2020-04-09 14:24:01 +0900

    Show number of steps executed and next instruction to execute.

--- eval-slow.js
+++ eval-slow.js
@@ -7,15 +7,20 @@ self.onmessage = function(e) {
     var t = e.data[2];
     var pos = 0;
     var new_pos = 0;
+    var n_steps = 0;
 
     var eval_loop = setInterval(function() {
         new_pos = step(p, pos, regs);
-	if (new_pos==pos) {
-	    clearInterval(eval_loop);
-	    self.postMessage([pos, regs, true]);
-	    self.close();
-	}
-	self.postMessage([pos, regs, false]);
-	pos = new_pos;
+        n_steps++;
+//	    if (new_pos == pos) {
+        if (new_pos < 0 || p.length <= new_pos) {
+	        clearInterval(eval_loop);
+//	        self.postMessage([pos, regs, true]);
+            self.postMessage([new_pos, regs, true, n_steps]);
+	        self.close();
+        }
+//	    self.postMessage([pos, regs, false]);
+        self.postMessage([new_pos, regs, false, n_steps]);
+        pos = new_pos;
     }, t);
 };
