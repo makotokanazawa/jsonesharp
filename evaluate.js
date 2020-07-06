c1ad17cdb3a308c794d2d20e27fbc35bf9606c92
makotokanazawa <37959854+makotokanazawa@users.noreply.github.com>
2020-07-06 21:38:18 +0900

    Update evaluate.js
    returns the number of steps executed

--- evaluate.js
+++ evaluate.js
@@ -4,8 +4,10 @@ self.onmessage = function(e) {
 
     var p = e.data[0];
     var regs = e.data[1];
-    var pos = onesharp(p, regs);
+//    var pos = onesharp(p, regs);
+    var result = onesharp(p, regs);
 
-    self.postMessage([pos, regs]);
+//    self.postMessage([pos, regs]);
+    self.postMessage([result[0], regs, result[1]]);
     self.close();
 };
